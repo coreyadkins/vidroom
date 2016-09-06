1e6bffe053cab98d8457adfbd6d8c6cc9c0aeefc
Corey Adkins <adkinsbass@gmail.com>
2016-09-06 10:16:07 -0700

    Added models, setting up Vidroom creation

--- vidroom/logic.py
+++ vidroom/logic.py
@@ -2,8 +2,22 @@
 
 import uuid
 import arrow
+from . import models
 
 # NODE = uuid.getnode()
 
 def create_uuid():
+    """Creates a UUID, or universally unique identifier, which will be used to identify and access the newly created
+    VidRoom."""
     return uuid.uuid4()
+
+
+def create_and_save_new_vidroom(vidroom_id):
+    """Takes a UUID, and creates a new vidroom with the """
+    new_vidroom = models.Vidroom(id=vidroom_id, playlist=[], start_at=0)
+    new_vidroom.save()
+
+
+def find_vidroom_by_uuid(vidroom_id):
+    """"""
+    return models.VidRoom.objects.get(uuid=vidroom_id)
