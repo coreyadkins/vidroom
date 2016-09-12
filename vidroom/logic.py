4b572ede2fafa6efd9ebb8c01a0769116f935a4d
Corey Adkins <adkinsbass@gmail.com>
2016-09-12 10:55:20 -0700

    Changes models to create new PlaylistEntry model, corrects testing for new models

--- vidroom/logic.py
+++ vidroom/logic.py
@@ -15,9 +15,9 @@ def create_and_save_new_vidroom(vidroom_id):
 
     >>> vidroom = create_and_save_new_vidroom('f81d4fae-7dec-11d0-a765-00a0c91e6bf6')
     >>> models.VidRoom.objects.get(public_id='f81d4fae-7dec-11d0-a765-00a0c91e6bf6')
-    VidRoom(public_id='f81d4fae-7dec-11d0-a765-00a0c91e6bf6', playlist='[]', start_at=0)
+    VidRoom(public_id='f81d4fae-7dec-11d0-a765-00a0c91e6bf6')
     """
-    new_vidroom = models.VidRoom(public_id=vidroom_id, playlist=[], start_at=0)
+    new_vidroom = models.VidRoom(public_id=vidroom_id)
     new_vidroom.save()
     return new_vidroom
 
@@ -25,9 +25,9 @@ def create_and_save_new_vidroom(vidroom_id):
 def find_vidroom_by_public_id(vidroom_id):
     """Returns the vidroom which contains the inputted public id number.
 
-    >>> models.VidRoom(public_id='f81d4fae-7dec-11d0-a765-00a0c91e6bf6', playlist=[], start_at=0).save()
+    >>> models.VidRoom(public_id='f81d4fae-7dec-11d0-a765-00a0c91e6bf6').save()
     >>> find_vidroom_by_public_id('f81d4fae-7dec-11d0-a765-00a0c91e6bf6')
-    VidRoom(public_id='f81d4fae-7dec-11d0-a765-00a0c91e6bf6', playlist='[]', start_at=0)
+    VidRoom(public_id='f81d4fae-7dec-11d0-a765-00a0c91e6bf6')
     """
     return models.VidRoom.objects.get(public_id=vidroom_id)
 
@@ -38,7 +38,7 @@ def create_and_save_new_event(vidroom, event_type, video_time):
 
     Returns the timestamp of the event to be
 
-    >>> models.VidRoom(public_id='f81d4fae-7dec-11d0-a765-00a0c91e6bf6', playlist=[], start_at=0).save()
+    >>> models.VidRoom(public_id='f81d4fae-7dec-11d0-a765-00a0c91e6bf6').save()
     >>> vidroom = models.VidRoom.objects.get(public_id='f81d4fae-7dec-11d0-a765-00a0c91e6bf6')
     >>> create_and_save_new_event(vidroom, 'play', 135.0)
     >>> vidroom_events_by_most_recent = models.Event.objects.filter(vidroom=vidroom, event_type='play', ).order_by(\
@@ -47,8 +47,8 @@ def create_and_save_new_event(vidroom, event_type, video_time):
     >>> default_time = datetime.datetime(1970, 1, 1, 0, 0)
     >>> most_recent_event.timestamp = default_time
     >>> most_recent_event
-    Event(vidroom=VidRoom(public_id='f81d4fae-7dec-11d0-a765-00a0c91e6bf6', playlist='[]', start_at=0), event_type='\
-play', video_time_at=135.0, timestamp=datetime.datetime(1970, 1, 1, 0, 0))
+    Event(vidroom=VidRoom(public_id='f81d4fae-7dec-11d0-a765-00a0c91e6bf6'), event_type='play', video_time_at=135.0, \
+timestamp=datetime.datetime(1970, 1, 1, 0, 0))
     """
     new_event = models.Event(vidroom=vidroom, event_type=event_type, video_time_at=video_time)
     new_event.save()
@@ -57,7 +57,7 @@ play', video_time_at=135.0, timestamp=datetime.datetime(1970, 1, 1, 0, 0))
 def find_events_by_vidroom(vidroom):
     """Returns all event objects associated with inputted vidroom, sorted in order of most recent by timestamp.
 
-    >>> models.VidRoom(public_id='f91d4fae-7dec-11d0-a765-00a0c91e6bf6', playlist=[], start_at=0).save()
+    >>> models.VidRoom(public_id='f91d4fae-7dec-11d0-a765-00a0c91e6bf6').save()
     >>> vidroom = models.VidRoom.objects.get(public_id='f91d4fae-7dec-11d0-a765-00a0c91e6bf6')
     >>> models.Event(vidroom=vidroom, event_type='play', video_time_at=135.0).save()
     >>> result = list(find_events_by_vidroom(vidroom))
@@ -65,7 +65,7 @@ def find_events_by_vidroom(vidroom):
     >>> result_event = result[0]
     >>> result_event.timestamp = default_time
     >>> result_event
-    Event(vidroom=VidRoom(public_id='f91d4fae-7dec-11d0-a765-00a0c91e6bf6', playlist='[]', start_at=0), event\
-_type='play', video_time_at=135.0, timestamp=datetime.datetime(1970, 1, 1, 0, 0))
+    Event(vidroom=VidRoom(public_id='f91d4fae-7dec-11d0-a765-00a0c91e6bf6'), event_type='play', video_time_at=135.0, \
+timestamp=datetime.datetime(1970, 1, 1, 0, 0))
     """
     return models.Event.objects.filter(vidroom=vidroom).order_by('-timestamp')
--- vidroom/models.py
+++ vidroom/models.py
@@ -7,14 +7,11 @@ import datetime
 
 class VidRoom(models.Model):
     public_id = models.TextField(default=str(uuid.uuid4))
-    playlist = models.URLField()
-    start_at = models.IntegerField()
 
     def __str__(self):
         """Returns str.
 
-        >>> str(VidRoom(public_id='123', playlist=['https://www.youtube.com/watch?v=ORjtrEW8whg', 'https://www.youtube.com/'\
-        'watch?v=tS_uCfBX1No'], start_at=135))
+        >>> str(VidRoom(public_id='123'))
         'VidRoom(123)'
         """
         return 'VidRoom({})'.format(self.public_id)
@@ -22,12 +19,10 @@ class VidRoom(models.Model):
     def __repr__(self):
         """Returns repr.
 
-        >>> repr(VidRoom(public_id='123', playlist=['https://www.youtube.com/watch?v=ORjtrEW8whg', 'https://www.youtube.com'\
-        '/watch?v=tS_uCfBX1No'], start_at=135))
-        "VidRoom(public_id='123', playlist=['https://www.youtube.com/watch?v=ORjtrEW8whg', 'https://www.youtube.com\
-/watch?v=tS_uCfBX1No'], start_at=135)"
+        >>> repr(VidRoom(public_id='123'))
+        "VidRoom(public_id='123')"
         """
-        return 'VidRoom(public_id={!r}, playlist={!r}, start_at={})'.format(self.public_id, self.playlist, self.start_at)
+        return 'VidRoom(public_id={!r})'.format(self.public_id)
 
 
 class Event(models.Model):
@@ -45,23 +40,45 @@ class Event(models.Model):
     def __str__(self):
         """Returns str.
         >>> time = datetime.datetime(2013, 5, 5, 12, 30, 45)
-        >>> vidroom = VidRoom(public_id='123', playlist=['https://www.youtube.com/watch?v=ORjtrEW8whg'], start_at=135)
+        >>> vidroom = VidRoom(public_id='123')
         >>> str(Event(vidroom=vidroom, event_type='Play', video_time_at=135, timestamp=time))
         'Event(2013-05-05 12:30:45: Play at 135 in VidRoom(123))'
         """
-        return 'Event({}: {} at {} in {})'.format(self.timestamp, self.event_type,
-                                                      self.video_time_at, self.vidroom)
+        return 'Event({}: {} at {} in {})'.format(self.timestamp, self.event_type, self.video_time_at, self.vidroom)
 
     def __repr__(self):
         """Returns repr.
 
         >>> time = datetime.datetime(2013, 5, 5, 12, 30, 45)
-        >>> vidroom = VidRoom(public_id='123', playlist=['https://www.youtube.com/watch?v=ORjtrEW8whg'], start_at=135)
+        >>> vidroom = VidRoom(public_id='123')
         >>> repr(Event(vidroom=vidroom, event_type='Pause', video_time_at=140, timestamp=time))
-        "Event(vidroom=VidRoom(public_id='123', playlist=['https://www.youtube.com/watch?v=ORjtrEW8whg'\
-], start_at=135), event_type='Pause', video_time_at=140, timestamp=datetime.datetime(2013, 5, 5, 12, 30, 45))"
+        "Event(vidroom=VidRoom(public_id='123'), event_type='Pause', video_time_at=140, timestamp=datetime.datetime(201\
+3, 5, 5, 12, 30, 45))"
         """
         return 'Event(vidroom={!r}, event_type={!r}, video_time_at={!r}, timestamp={!r})'.format(self.vidroom,
                                                                                                     self.event_type,
                                                                                                     self.video_time_at,
                                                                                                     self.timestamp)
+
+class PlaylistEntry(models.Model):
+    """"""
+    vidroom = models.ForeignKey(VidRoom)
+    url = models.URLField()
+
+    def __str__(self):
+        """Returns str.
+
+        >>> vidroom = VidRoom(public_id='123')
+        >>> str(PlaylistEntry(vidroom=vidroom, url='https://www.youtube.com/watch?v=4B9NtFlES4U'))
+        'PlaylistEntry(VidRoom(123), https://www.youtube.com/watch?v=4B9NtFlES4U)'
+        """
+        return 'PlaylistEntry({}, {})'.format(self.vidroom, self.url)
+
+    def __repr__(self):
+        """Returns repr.
+
+        >>> vidroom = VidRoom(public_id='123')
+        >>> repr(PlaylistEntry(vidroom=vidroom, url='https://www.youtube.com/watch?v=4B9NtFlES4U'))
+        "PlaylistEntry(vidroom=VidRoom(public_id='123'), url='https://www.youtube.com/watch?v=4B9NtFlES4U')"
+        """
+        return 'PlaylistEntry(vidroom={!r}, url={!r})'.format(self.vidroom, self.url)
